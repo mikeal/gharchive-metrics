ae1ba30f69ece84f3f858a6762bedd7679817d94
Mikeal Rogers <mikeal.rogers@gmail.com>
2019-02-11 05:56:02 +0000

    fix: working filter to cache file

--- src/http/get-filter/index.js
+++ src/http/get-filter/index.js
@@ -2,52 +2,65 @@
 // let arc = require('@architect/functions')
 // let url = arc.http.helpers.url
 
-const filter = require('@architect/shared/s3')
+const createS3 = require('@architect/shared/s3')
+const s3 = createS3()
+const cbor = require('dag-cbor-sync')()
+const { createGzip } = require('zlib')
 
-exports.handler = async function http(req) {
-  console.log(req)
+const { Transform } = require('stream')
+
+const geturl = req => {
   let proto = req.headers['X-Forwarded-Proto']
   let host = req.headers.Host
   let env = process.env.NODE_ENV
   let url = `${proto}://${host}/${env}`
-  return {
-    type: 'text/html; charset=utf8',
-    body: `<pre>${JSON.stringify({url}, null, 2)}</pre>`
-  }
+  return url
 }
 
-// Example responses
-
-/* Forward requester to a new path
-exports.handler = async function http(request) {
-  if (process.env.NODE_ENV !== 'production') {
-    console.log(request)
-  }
-  return {
-    status: 302,
-    location: '/staging/about',
-  }
+exports.handler = async function http(req) {
+  console.log(req)
+  let url = geturl(req)
+  let {filter, file} = req.query
+  let {orgs, repos, keys} = cbor.deserialize(await s3.getObject(`blocks/${filter}`)) 
+  keys.push('repo.name') 
+  keys.push('created_at')
+  keys = Array.from(new Set(keys))
+  keys = keys.map(k => 's.' + k).join(', ')
+  let sql = `SELECT ${keys} from S3Object s`
+  let query = await s3.query(sql, `gharchive/${file}`)
+  let uploader = s3.upload(`cache/${filter}/${file}`)
+  let _f = r => {
+    if (r.name) {
+      // has a repo name
+      for (let repo of repos) {
+        if (r.name === repo) return true
+      }
+      for (let org of orgs) {
+        if (r.name.startsWith(`${org}/`)) return true
+      }
+    }
+  } 
+  const _transform = new Transform({
+    writableObjectMode: true,
+    objectMode: true,
+    transform(obj, encoding, callback) {
+      if (_f(obj)) {
+        callback(null, JSON.stringify(obj) + '\n')
+      } else {
+        callback(null)
+      }
+    }
+  })
+  query.pipe(_transform).pipe(createGzip()).pipe(uploader)
+  return new Promise((resolve, reject) => {
+    uploader.on('finish', () => resolve({
+      type: 'application/json',
+      body: JSON.stringify({
+        cache: `cache/${filter}/${file}`,
+        sql, orgs, repos, keys, filter, file
+      })
+    }))
+    uploader.on('error', reject)
+  })
 }
-*/
 
-/* Successful resource creation, CORS enabled
-exports.handler = async function http(request) {
-  return {
-    status: 201,
-    type: 'application/json',
-    body: JSON.stringify({ok: true}),
-    cors: true,
-  }
-}
-*/
-
-/* Deliver client-side JS
-exports.handler = async function http(request) {
-  return {
-    type: 'text/javascript',
-    body: 'console.log("Hello world!")',
-  }
-}
-*/
-
-// Learn more: https://arc.codes/guides/http
